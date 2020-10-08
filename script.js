b757e2b65866a54da84bed8859923b5d203bb297
John Mieszkowski <jmm3@njit.edu>
2020-10-08 13:22:38 -0400

    Playthrough functionality added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 //Global variables:
 let gamesPlayed =0; //running tally of number of games played
 let turnsPlayed =0; //running tally of number of turns in a given game
-let whoseTurn = 'x' //keeps track of whose turn. X always begins a new game
+let whoseTurn = ['x','o','x','o','x','o','x','o','x'] //keeps track of whose turn. X always begins a new game.
+let xChoices = [];
+let oChoices = [];
+
 let gameInProgress = false; //
+let winners = ['abc', 'def', 'ghi', 'aei', 'ceg', 'adg', 'beh', 'cfi'] //Sorted alphabetically
+
 
 
 //When New Game button is selected, a new game is initiated
@@ -22,11 +27,28 @@ function startGame(){
 
 function takeTurn(selectedButton){
     
-   selectedButton.setAttribute('style','background-color: rgb(230, 210, 182);');
-   turnsPlayed++;
-    console.log(`${selectedButton.id} works with ${turnsPlayed} games played!`)
+    if(!gameInProgress){
+        //alert('Click on New Game to start');
+        startGame();
+        //Alternatively, run startGame();
+        return;
+    }
+    else{
 
-    if(turnsPlayed=='9'){
+        if(whoseTurn[turnsPlayed]=='x')
+        {
+            selectedButton.setAttribute('style','background-color: rgb(230, 210, 182);');
+            //recordTurn(x, selectedButton.id);
+        }
+        else{
+            selectedButton.setAttribute('style','background-color: rgb(0, 0, 255);');
+            //recordTurn(o, selectedButton.id);
+        }
+   turnsPlayed++;
+   
+    console.log(`${selectedButton.id} works with ${turnsPlayed} turns played!`)
+
+    if(turnsPlayed>='9'){
         finishGame();
     }
     else{
@@ -35,19 +57,40 @@ function takeTurn(selectedButton){
     //return turnsPlayed;
 }
 
-function finishGame(){
-    console.log('game over');
-    gamesPlayed++;
 }
-// const buttonA=document.querySelector("#a");
-// const buttonB=document.querySelector("#b");
-// const buttonC=document.querySelector("#c");
-// const buttonD=document.querySelector("#d");
-// const buttonE=document.querySelector("#e");
-// const buttonF=document.querySelector("#f");
-// const buttonG=document.querySelector("#g");
-// const buttonH=document.querySelector("#h");
-// const buttonI=document.querySelector("#i");
+
+function finishGame(player){
+    console.log(`Game over. ${player} wins!`);
+    gamesPlayed++;
+    turnsPlayed=0;
+}
+
+function recordTurn(player, choice){
+switch (player){
+    case 'x':
+        xChoice[turnsPlayed] = choice;
+        xChoice.sort();
+        validateWinner(xChoice);
+    break;
+    
+    case 'o':
+        oChoice[turnsPlayed] = choice;
+        oChoice.sort();
+        validateWinner(oChoice);
+    break;
+    
+        
+}
+}
+
+function validateWinner(player){
+    for(let i=0; i<winners.length; i++){
+        if(player==winners[i]){
+            finishGame(player);
+        }
+        else{}
+    }
+}
 
 //Below initiate the sequence of events when a particular tile is clicked:
 document.querySelector('#a').addEventListener('click',() =>{takeTurn(a);});
@@ -57,5 +100,6 @@ document.querySelector('#d').addEventListener('click',() =>{takeTurn(d);});
 document.querySelector('#e').addEventListener('click',() =>{takeTurn(e);});
 document.querySelector('#f').addEventListener('click',() =>{takeTurn(f);});
 document.querySelector('#g').addEventListener('click',() =>{takeTurn(g);});
-document.querySelector('#h').addEventListener('click',() =>{takeTurn(h);})
+document.querySelector('#h').addEventListener('click',() =>{takeTurn(h);});
 document.querySelector('#i').addEventListener('click',() =>{takeTurn(i);});
+
